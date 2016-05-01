a5ece8c103acf382c17d8089abf0a17b1e496880
Stefan Caraiman <scaraiman@cloudbasesolutions.com>
2016-05-01 21:16:44 +0300

    Add functions to part 2

--- Basics/part2.py
+++ Basics/part2.py
@@ -5,3 +5,21 @@ from __future__ import print_function
 from math import sqrt
 
 print(sqrt(121))
+
+def f():
+	return "Hello world!"
+
+
+def f2():
+	def f():
+		return "Goodbye world!"
+	print(f())
+
+def to_add(nr):
+	def sum(number):
+		return nr + number
+	return sum
+
+f2()
+add_5_to = to_add(5)
+print(add_5_to(36))
